fd5cf42b7fa787af0cbc7f271c8996444e928e49
josebp22 <34610013+josebp22@users.noreply.github.com>
2021-03-15 09:22:57 +0000

    Update Circle-v1.py
    v2

--- Circle-v1.py
+++ Circle-v1.py
@@ -4,8 +4,10 @@ pi=math.pi
 radius=int(input("What is your circle radius: "))
 
 circumference=pi*radius*2
+area_of_circle=pi*radius*radius
 
 
 
-print(f"Area of Circle= {round(circumference,2)}")
+print(f"Area of Circle= {round(area_of_circle,2)} \U0001F61B" + 3*"\U0001F61B")
+print(f"Circumference= {round(circumference,2)} \U0001F61B" + 3*"\U0001F61B")
 
